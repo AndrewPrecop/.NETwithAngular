3ef1de9a13207447f47ee56f92c7afda7ac7cd4e
Andrew8991 <andy.andrew19988@gmail.com>
2020-03-11 11:31:39 +0200

    Backend Configurator PC application

---
+++ Core/Commands/ConfigurationPC/AddConfigurationPcCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Commands.ConfigurationPC
+{
+    public class AddConfigurationPCCommand
+    {   public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Guid> Components { get; set; }
+    }
+}
